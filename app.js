e9066f3dff749a40f544713b1c348a8f421b29ef
VIDUSHI GUPTA <vidugpt987@gmail.com>
2021-05-07 10:59:24 +0530

    Search, populate table using filter

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+//IIFE
 (function(){
     'use-strict';
+    //application module
     let app= angular.module('angularTable' , []);
+    //controller module
     app.controller('tableCtrl', function($scope){
         $scope.query = {}
   $scope.queryBy= "$"
@@ -8,31 +11,31 @@
         name: "Martin",
         email: "martin@patientbond.com",
         phone: "1234567890",
-        gender: "male",
+       image: "fas fa-male",
         age: "24"
       }, {
         name: "Harry",
         email: "harry@hotmail.com",
         phone: "456745678",
-        gender: "male",
+        image: "fas fa-male",
         age: "22"
       }, {
         name: "Pat",
         email: "pat@hotmail.com",
         phone: "3435344535",
-        gender: "male",
+        image: "fas fa-male",
         age:"21"
       }, {
         name: "Alex",
         email: "alex@hotmail.com",
        phone: "0987654321",
-        gender: "Male",
+       image: "fas fa-male",
         age:"23"
       }, {
         name: "Nora Smith",
         email: "nora@hotmail.com",
         phone: "6754890876",
-        gender: "female",
+        image: "fas fa-female",
         age: "23"
       }, ];
   
